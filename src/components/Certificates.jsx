f0cb6aa82babe352438e1f7dcd55f8af85146e5a
Antônio Vítor Bozzo de Napoli <antonionapoli394@gmail.com>
2025-10-21 10:22:16 -0300

    deploy

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useIntersectionObserver } from '../hooks/UseIntersectionObserver';
+import { useIntersectionObserver } from '../hooks/useIntersectionObserver';
 
 const certificates = [
   {
